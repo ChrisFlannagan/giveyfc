710c4c6b5979e250d42fa8e2937be2db87054623
Christopher Flannagan <chris@flowpress.com>
2017-02-28 20:34:54 -0500

    Fixed icon situation for future builds Updated logo and branding in pinfo

--- platforms/ios/giveyfc/app/views/donate/donate.js
+++ platforms/ios/giveyfc/app/views/donate/donate.js
@@ -1,18 +1,24 @@
-var webViewInterfaceModule = require('nativescript-webview-interface');
+var utilityModule = require("utils/utils");
+// var webViewInterfaceModule = require('nativescript-webview-interface');
 var oWebViewInterface;
 var appSettings = require("application-settings");
 var md5 = require('js-md5');
 
 exports.loaded = function(args){
     page = args.object;
+    page.bindingContext = { };
     setupWebViewInterface(page)
-}
+};
 
 // Initializes plugin with a webView
 function setupWebViewInterface(page){
+    utilityModule.openUrl(global.giveurl + "/give-app-api/donate/" +
+        appSettings.getString("userID") + "/" + appSettings.getString("giveID") + "/" + appSettings.getString("userPin"));
+    /*
     console.log(appSettings.getString("giveID"));
     var webView = page.getViewById('webView');
     console.log(global.giveurl + "/give-app-api/donate/" + appSettings.getString("userID") + "/" + appSettings.getString("giveID"));
     oWebViewInterface = new webViewInterfaceModule.WebViewInterface(webView, global.giveurl + "/give-app-api/donate/" +
         appSettings.getString("userID") + "/" + appSettings.getString("giveID") + "/" + appSettings.getString("userPin"));
-}
+        */
+};
